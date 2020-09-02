b0e04e4413efb669aaeca6a6ea6e496115edee69
Jacques v.d Horst <demonix77x@gmail.com>
2020-09-02 11:42:47 +0200

    - Updated Template

--- no-header-footer.php
+++ no-header-footer.php
@@ -1,1 +1,48 @@
-<?php /* Template Name: No Header & Footer */ ?>
+<?php
+/**
+ * Template Name: No Header & Footer
+ *
+ * @package lsx
+ */
+?>
+
+<?php lsx_content_wrap_before(); ?>
+
+<div id="primary" class="content-area <?php echo esc_attr( lsx_main_class() ); ?>">
+
+	<?php lsx_content_before(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php lsx_content_top(); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+			while ( have_posts() ) :
+				the_post();
+			?>
+
+				<?php get_template_part( 'partials/content', 'page' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php endif; ?>
+
+		<?php lsx_content_bottom(); ?>
+
+	</main><!-- #main -->
+
+	<?php lsx_content_after(); ?>
+
+	<?php
+	if ( comments_open() ) {
+		comments_template();
+	}
+	?>
+
+</div><!-- #primary -->
+
+<?php lsx_content_wrap_after(); ?>
+
+<?php get_sidebar(); ?>
